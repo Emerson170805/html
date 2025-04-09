1861229c6ff4a015f9ec95763b35f9699b382fde
Emerson170805 <1555449@senati.pe>
2025-04-09 10:59:59 -0500

    terminado

--- emociones-detecor/main.py
+++ emociones-detecor/main.py
@@ -4,17 +4,17 @@ import tensorflow as tf
 import mediapipe as mp
 
 def main():
-    # Cargar modelo entrenado
+    # CARGANDO EL MODELO DEL ENTRENAMIENTO
     modelo = tf.keras.models.load_model('modelo_emociones.h5')
     etiquetas = ['feliz', 'neutral', 'sorprendido', 'triste']
 
-    # Iniciar captura con cámara 2
+    # VA A UTILIZAR LA CAMARA 2
     cap = cv2.VideoCapture(2)
     if not cap.isOpened():
         print("❌ No se pudo abrir la cámara.")
         return
 
-    # Inicializar FaceMesh de MediaPipe
+    # INICIA LA LIBRERIA MEDIAPIPE
     mp_face_mesh = mp.solutions.face_mesh
     face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=5, min_detection_confidence=0.5)
 
@@ -47,15 +47,15 @@ def main():
                     pred = modelo.predict(entrada, verbose=0)
                     emocion = etiquetas[np.argmax(pred)]
 
-                    # Dibujar malla facial
+                    # USA MALLA FACIAL
                     for punto in puntos:
                         cv2.circle(frame, punto, 1, (0, 255, 0), -1)
 
-                    # Mostrar emoción y recuadro
+                    # VA A MOSTRAR LA EMOCION EN ARRIBA DEL RECUADRO
                     cv2.putText(frame, emocion, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
 
-            # Mostrar solo una ventana
+            # ASEGURAR QUE SE HABRA UNA VENTANA NOMA
             cv2.imshow("Detector de emociones", frame)
             if cv2.waitKey(1) & 0xFF == 27:  # ESC para salir
                 break
